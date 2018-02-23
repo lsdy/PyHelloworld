64174669a1386ce10bb44b0be78b0937f99d397e
jacket <jacket.1995@qq.com>
2018-02-23 15:12:05 +0800

    modified:
    1.add comment only for Cpp

--- src/step1/hello.py
+++ src/step1/hello.py
@@ -6,12 +6,19 @@ Created on 2018年2月8日
 @author: jacket
 '''
 import os
-import sys
+import re
+
+CppSuffixList=['cpp','c','cc','hpp','h']
 
 #file type
 FILE=1
 DIR=0
 UNKOWN=-1
+
+#获得文件的决定路径
+def getAbsFilePath(inFilePath):
+    return os.path.abspath(inFilePath)
+
 
 def openFile(filename):
     f=open(filename,'r')
@@ -22,8 +29,16 @@ def openFile(filename):
 
 #判断是否以及存在注释
 def isContainCommentForCPP(fileText):
-    curline=fileText.readline()
-    
+#     re.match(pattern, string, flags)
+    return True
+
+#判断是否为Cpp的文件0     
+def isCppFile(filename):
+    for suffix in CppSuffixList:
+        if(filename.endswith('.'+suffix)):
+            return True
+    return False
+
 #修改CPP或C类型的注释
 def modifyCPPFile(filename,comment):
 #     print(filename)
@@ -41,37 +56,21 @@ def fileType(filepath):
         print('Unkown file :'+filepath)
         return UNKOWN
 
-def modifyFileRecursion(destPath):
+
+def modifyFileRecursion(destPath,srcFile,comment):
     all_file = os.listdir(destPath)
     # print(all_file)
     for each_file in all_file:
-        #由是否进位决定是否还在for循环内
-    #     print(destPath+each_file)
-    #     print(srcFile)
         OneDir=destPath+each_file;
         
         filetype=fileType(OneDir)
         if filetype==FILE:
             if srcFile!=OneDir:
-                print 'Modify '+OneDir
-                modifyCPPFile(OneDir,comment)
+                print OneDir
+                if isCppFile(OneDir):
+                    print 'Modify '+OneDir
+                    modifyCPPFile(OneDir,comment)
         elif filetype==DIR:
-            modifyFileRecursion(OneDir+'/')
+            modifyFileRecursion(OneDir+'/',srcFile,comment)
             
-#修改路径
-all_argv=sys.argv
-# print(all_argv)
-if len(all_argv) < 3 :
-    print 'Argments Input Wrong!'
-    sys.exit(1)
-
-destPath=sys.argv[1]
-# print(destPath)
-srcFile=sys.argv[2]
-# print(srcFile)
-
-comment=openFile(srcFile)
-
-modifyFileRecursion(destPath)
-
-sys.exit(0)
+            
