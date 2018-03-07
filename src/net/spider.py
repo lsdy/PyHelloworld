72611ff31e9d5407e737a4f77f3ce5f1b34ff02e
jacket <jacket.1995@qq.com>
2018-03-07 11:39:44 +0800

    In windows,type is "mbcs" ,which makes messy

--- src/net/spider.py
+++ src/net/spider.py
@@ -9,10 +9,11 @@ import urllib2
 import sys
 
 type=sys.getfilesystemencoding()
+print type
 url="http://www.shenpinwu.com/7/7311/"
 
 response=urllib2.urlopen(url)
 html=response.read()
-htmlWithutf8=html.decode("gbk").encode(type)
+htmlWithutf8=html.decode("gbk")
 
 print htmlWithutf8
