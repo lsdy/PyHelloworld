7b078240cfa63b7619d39a772e1a004156be1bba
jacket <jacket.1995@qq.com>
2018-03-09 17:30:17 +0800

    save

--- src/net/spider.py
+++ src/net/spider.py
@@ -6,16 +6,35 @@ Created on 2018年3月5日
 @author: jacket
 '''
 import urllib2
+import urllib
 import sys
 from bs4 import BeautifulSoup
 
 
+def recurse(node):
+    a=type(node)
+#     print a
+    if(str(a)=="<class 'bs4.element.Tag'>"):
+#         print node.name
+        for child in node:
+            recurse(child)
+    elif (str(a)=="<class 'bs4.element.NavigableString'>"):
+       print node.string
+       print node
+        
+
+                
 def main():
     # type=sys.getfilesystemencoding()
     # print type #在windows下编码有问题，不对其进行编码了
     url="http://www.shenpinwu.com/7/7311/"
-    
-    response=urllib2.urlopen(url)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1'}
+#     values = {'username' : 'cqc',  'password' : 'XXXX' }
+#     data = urllib.urlencode(values) 
+#     print data
+    data=None
+    request=urllib2.Request(url,data,headers)
+    response=urllib2.urlopen(request)
     html=response.read()
 #     print html
     soup=BeautifulSoup(html,"lxml")
@@ -24,8 +43,15 @@ def main():
     # print type(soup.html.meta) #<class 'bs4.element.Tag'>获得其在python的类型
     # print soup.html.name.name #如果xml有个标记<name></name>那会发生不能找到该节点的错误，因为已经被Tag的name属性占用
 #     print soup #bs4已经完成转码了
-    print soup.html.head.meta
-    print soup.html.head.meta.attrs
+#     print soup.html.center
+#     print soup.html.head.meta.attrs
+    start=soup.html.body.center
+    print type(start)
+    for child in start:
+#         print child
+#         print "==========================================="
+        recurse(child)
+     
     
 if __name__=='__main__':
     main()
